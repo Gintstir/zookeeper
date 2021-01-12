2ac51a1efcab0437ee7ced0d83cd3995f689970e
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-01-12 10:11:09 -0500

    added req.param to seach for specific id

--- server.js
+++ server.js
@@ -48,6 +48,11 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 //add the route
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -55,6 +60,18 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+/*
+ an additions .get route using req.params : app.get('<route>/:<parameterName>',...
+ paramater routes MUST come AFTER the other .get route.  
+ */
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if(result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }        
 });
 
 app.listen(PORT, () => {
