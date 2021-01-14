1882be252f51097e51b461c7f2cb443a6312bc8d
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-01-14 16:38:37 -0500

    fixing errors

--- public/assets/js/animals.js
+++ public/assets/js/animals.js
@@ -31,18 +31,17 @@ const getAnimals = (formData = {}) => {
 
   console.log(queryUrl);
 
-  //standard fetch() usage for making a GET request
   fetch(queryUrl)
-  .then(response => {
-    if (!response.ok) {
-      return alert('Error: ' + response.statusText);
-    }
-    return response.json();
-  })
-  .then(animalData => {
-    console.log(animalData);
-    printResults(animalData);
-  });
+    .then(response => {
+      if (!response.ok) {
+        return alert('Error: ' + response.statusText);
+      }
+      return response.json();
+    })
+    .then(animalData => {
+      console.log(animalData);
+      printResults(animalData);
+    });
 };
 
 const handleGetAnimalsSubmit = event => {
--- server.js
+++ server.js
@@ -1,28 +1,18 @@
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
-
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-
-//parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-
-//parse incoming JSON data
 app.use(express.json());
-
-//midcleware that tells the server to look in the public folder for the js and css files?
-//instructs the server to make certain files readily available and to not gate them 
-//behind a server endpoint: 
 app.use(express.static('public'));
 
+// Use apiRoutes
 app.use('/api', apiRoutes);
-
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
